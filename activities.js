32cb01dfe0e796a951b9420b3b9fdebf2da27137
Andy Jacko <wisdom.mcr@gmail.com>
2022-09-02 13:39:57 +0100

    Activities added...

--- activities.js
+++ activities.js
@@ -1,1 +1,97 @@
-// Activities Will Go Here
+// Create A Pet
+
+// const pet = {
+//   name: "Biscuit",
+//   typeOfPet: "Dog",
+//   age: 10,
+//   colour: "Ginger",
+//   eat: function () {
+//     return `${this.name} is eating...`;
+//   },
+//   drink: function () {
+//     return `${this.name} is drinking...`;
+//   },
+// };
+
+// console.log(pet.eat());
+// console.log(pet.drink());
+
+// ------------------------------------------------------------------------------
+
+// 6 Random Numbers Divisible By 7
+
+// for (let i = 0; i < 6; i++) {
+//   let num = Math.ceil(Math.random() * 30);
+//   if (num % 7 === 0) {
+//     console.log(`${num} IS divisible by 7`);
+//   } else {
+//     console.log(`${num} is NOT divisible by 7`);
+//   }
+// }
+
+// ------------------------------------------------------------------------------
+
+// Adding Method To Person Object To Log Hi Message
+
+// const person = {
+//   name: "Slim Shady",
+//   age: 49,
+//   sayHi: function () {
+//     return `Hi! My name is...What! My name is...Who! My name is (jigga jigga) ${this.name}!`;
+//   },
+// };
+
+// console.log(person.sayHi());
+
+// ------------------------------------------------------------------------------
+
+// 6 Random Numbers Between 1-50
+
+// for (let i = 0; i < 6; i++) {
+//   console.log(Math.ceil(Math.random() * 50));
+// }
+
+// ------------------------------------------------------------------------------
+
+// Count Down From 9 to 0
+
+// for (let i = 9; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// ------------------------------------------------------------------------------
+
+// What Are We Eating Today
+
+// let breakfast = "Cornflakes",
+//   lunch = "BLT Sandwich",
+//   dinner = "Hawaiian Pizza";
+
+// console.log(
+//   `\nToday's Menu\n------------\nBreakfast: ${breakfast}\nLunch: ${lunch}\nDinner: ${dinner}`
+// );
+
+// breakfast = "Beans On Toast";
+// lunch = "Cheese & Onion Pasty";
+// dinner = "Fish & Chips";
+
+// console.log(
+//   `\nTomorrows's Menu\n----------------\nBreakfast: ${breakfast}\nLunch: ${lunch}\nDinner: ${dinner}`
+// );
+
+// ------------------------------------------------------------------------------
+
+// Favourite Movies
+
+// let movies = [
+//   "Shaun Of The Dead",
+//   "12 Angry Men",
+//   "Star Wars: A New Hope",
+//   "Ted",
+// ];
+
+// movies.push("Alan Partridge: Alpha Papa", "Chopper");
+
+// movies.forEach((movie) => {
+//   console.log(movie);
+// });
